1ca11ed0b909d9f4653d31ae3a9485477f431a80
JMorin3023 <JMorin3023@conestogac.on.ca>
2020-03-03 11:42:16 -0500

    Added assignment example files, started working on menu

---
+++ A3ExampleCode/A3ExampleCode/Ball.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ExampleCode
+{
+    class Ball
+    {
+        private string ballName;
+
+        public string BallName
+        {
+            get { return ballName; }
+            set { ballName = value; }
+        }
+
+        private int ballHealth;
+
+        public int BallHealth
+        {
+            get { return ballHealth; }
+            set { ballHealth = value; }
+        }
+
+        private string ballAttackOneName;
+        private int ballAttackOneDamage;
+
+        public string BallAttackOneName
+        {
+            get { return ballAttackOneName; }
+            set { ballAttackOneName = value; }
+        }
+        public int BallAttackOneDamage
+        {
+            get { return ballAttackOneDamage; }
+            set { ballAttackOneDamage = value; }
+        }
+
+        private string ballAttackTwoName;
+        private int ballAttackTwoDamage;
+
+        public string BallAttackTwoName
+        {
+            get { return ballAttackTwoName; }
+            set { ballAttackTwoName = value; }
+        }
+        public int BallAttackTwoDamage
+        {
+            get { return ballAttackTwoDamage; }
+            set { ballAttackTwoDamage = value; }
+        }
+
+        private string ballAttackThreeName;
+        private int ballAttackThreeDamage;
+
+        public string BallAttackThreeName
+        {
+            get { return ballAttackThreeName; }
+            set { ballAttackThreeName = value; }
+        }
+        public int BallAttackThreeDamage
+        {
+            get { return ballAttackThreeDamage; }
+            set { ballAttackThreeDamage = value; }
+        }
+
+        public string takeDamage(int amountDamage)
+        {
+            BallHealth -= amountDamage;
+
+            if (BallHealth < 0)
+            {
+                return ballName + " has " + BallHealth + " Health left";
+            }
+            else
+            {
+                return ballName + " is dead";
+            }
+        }
+
+        public int getAttackParameters(string attackName)
+        {
+            if (attackName == ballAttackOneName)
+            {
+                return BallAttackOneDamage;
+            }
+            if (attackName == ballAttackTwoName)
+            {
+                return BallAttackTwoDamage;
+            }
+            if (attackName == ballAttackThreeName)
+            {
+                return BallAttackThreeDamage;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
